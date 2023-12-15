ef01553c25e0558c4c83cbace93f04c76ad23edd
Your Name <you@example.com>
2023-12-15 18:39:51 -0500

    until json

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
-use clap::{App, Arg};
-use std::env;
+mod to_do;
+use crate::to_do::traits::delete::Delete;
+use crate::to_do::traits::edit::Edit;
+use crate::to_do::traits::get::Get;
+use to_do::enums::TaskStatus;
+use to_do::to_do_factory;
+use to_do::ItemTypes;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let done: ItemTypes = to_do::to_do_factory("shopping", TaskStatus::PENDING);
+    match (done) {
+        ItemTypes::Done(done_task) => {
+            done_task.get(&done_task.super_struct.title);
+            done_task.delete(&done_task.super_struct.title);
+        }
+        ItemTypes::Pending(pending_task) => {
+            pending_task.get(&pending_task.super_struct.title);
+            pending_task.set_to_done(&pending_task.super_struct.title);
+        }
+    }
 }
---
+++ src/state.rs
@@ -0,0 +1,11 @@
+use std::fs::File;
+
+use std::fs;
+
+use std::io::Read;
+
+use serde_json::Map;
+
+use serde_json::value::Value;
+
+use serde_json::json;
