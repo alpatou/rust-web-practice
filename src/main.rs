30016fcef0a4d9e74af20f6b46686734d55207d0
l l <l@l.l>
2023-12-18 19:24:22 +0200

    write read

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod state;
+use serde_json::value::Value;
+use serde_json::{json, Map};
+use state::{read_file, write_to_file};
+use std::env;
 mod to_do;
 use crate::to_do::traits::delete::Delete;
 use crate::to_do::traits::edit::Edit;
@@ -18,4 +23,17 @@ fn main() {
             pending_task.set_to_done(&pending_task.super_struct.title);
         }
     }
+
+    let args: Vec<String> = env::args().collect();
+    let status: &String = &args[1];
+    let title: &String = &args[2];
+    let file_path: String = String::from("./state.json");
+    let mut state: Map<String, Value> = read_file(&file_path);
+
+    println!("Before ops state was {:?}", state);
+    state.insert(title.to_string(), json!(status));
+
+    println!("After operation: {:?}", state);
+
+    write_to_file(&file_path, &mut state);
 }
